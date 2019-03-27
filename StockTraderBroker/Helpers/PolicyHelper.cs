80c7b40c71e92cfad76198a0df12fcc04a178f2b
Anders Meidahl <Andersmeidahl@gmail.com>
2019-03-27 19:12:05 +0100

    Stoped retying 3 times

--- StockTraderBroker/Helpers/PolicyHelper.cs
+++ StockTraderBroker/Helpers/PolicyHelper.cs
@@ -2,20 +2,22 @@
 using Flurl.Http;
 using Polly;
 using Polly.Retry;
+using Polly.Timeout;
 
 namespace StockTraderBroker.Helpers
 {
     public class PolicyHelper
     {
-        public static AsyncRetryPolicy ThreeRetriesAsync()
+        public static AsyncTimeoutPolicy ThreeRetriesAsync()
         {
-            return Policy.Handle<FlurlHttpException>()
-                .WaitAndRetryAsync(new[]
-                {
-                    TimeSpan.FromSeconds(1),
-                    TimeSpan.FromSeconds(2),
-                    TimeSpan.FromSeconds(3),
-                });
+            return Policy.TimeoutAsync(TimeSpan.FromSeconds(120));
+            //.Handle<FlurlHttpException>()
+            //.WaitAndRetryAsync(new[]
+            //{
+            //    TimeSpan.FromSeconds(1),
+            //    TimeSpan.FromSeconds(2),
+            //    TimeSpan.FromSeconds(3),
+            //});
         }
     }
 }
