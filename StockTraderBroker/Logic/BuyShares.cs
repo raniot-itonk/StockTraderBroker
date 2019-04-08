4a2c77d466ad9131b4cae60df9e3ee16a4053c92
Anders Meidahl <Andersmeidahl@gmail.com>
2019-04-08 22:51:49 +0200

    Updated

--- StockTraderBroker/Logic/BuyShares.cs
+++ StockTraderBroker/Logic/BuyShares.cs
@@ -72,7 +72,7 @@ namespace StockTraderBroker.Logic
             buyRequestInput.AmountOfShares -= sharesToBuy;
             await _transaction.CreateTransactionAsync(sellRequest.Price, sharesToBuy, sellRequest.AccountId, buyRequestInput.ReserveId, buyRequestInput.AccountId, buyRequestInput.StockId);
             _logger.LogWarning($"This is Anders's test {sharesToBuy} and {buyRequestInput.AmountOfShares}");
-            if (sharesToBuy == buyRequestInput.AmountOfShares) await _bankClient.RemoveReservation(buyRequestInput.ReserveId, "jwtToken");
+            if (buyRequestInput.AmountOfShares == 0) await _bankClient.RemoveReservation(buyRequestInput.ReserveId, "jwtToken");
 
             var ownershipRequest = new OwnershipRequest
             {
